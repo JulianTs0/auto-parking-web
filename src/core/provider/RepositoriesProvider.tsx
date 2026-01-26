2cad52f18846fbae38bde8b23232d91adfa88972
JulianTs0 <juliantorresssr@gmail.com>
2026-01-26 18:35:50 -0300

    add: providers

---
+++ src/core/provider/RepositoriesProvider.tsx
@@ -0,0 +1,38 @@
+import type { ReactNode } from "react";
+import { createContext, useContext, useMemo } from "react";
+import {  } from "../../infrastructure";
+
+interface RepositoriesProviderProps {
+  children: ReactNode;
+}
+
+interface RepositoriesContextType {
+}
+
+// dependecy context
+const RepositoriesContext = createContext<RepositoriesContextType | null>(null);
+
+export const RepositoriesProvider = (
+    { children }: RepositoriesProviderProps
+) => {
+  
+    const repositories = useMemo(() => ({
+    }), []);
+
+    // dependecy injection
+    return (
+        <RepositoriesContext.Provider value={repositories}>
+            {children}
+        </RepositoriesContext.Provider>
+    );
+
+};
+
+export const useRepositories = () => {
+
+    const context = useContext(RepositoriesContext);
+  
+    if (!context) throw new Error("context error");
+
+    return context;
+};
