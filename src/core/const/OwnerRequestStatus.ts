6352c8e51c6ec2c8387e2dd38c3f319ddbb2759a
JulianTs0 <juliantorresssr@gmail.com>
2026-03-14 15:22:16 -0300

    add: owner request status & fix: index file

---
+++ src/core/const/OwnerRequestStatus.ts
@@ -0,0 +1,6 @@
+export class OwnerRequestStatus {
+    public static readonly PENDING = 'Pending';
+    public static readonly APPROVED = 'Approved';
+    public static readonly REJECTED = 'Rejected';
+    public static readonly COMPLETED = 'Completed';
+}
