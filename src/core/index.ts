edde07fed2a1e95b9144c3b7b9fd6e6176ad8175
JulianTs0 <juliantorresssr@gmail.com>
2026-03-23 19:25:41 -0300

    fix: index file

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,2 +1,5 @@
 export * from './adapters/http-client';
 export * from './adapters/env';
+export * from './utils/icon-mapper';
+export * from './utils/image-mapper';
+export * from './provider/RepositoriesProvider';
