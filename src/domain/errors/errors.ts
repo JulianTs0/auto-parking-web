e11b62c5257755c1f08fcb5197cdae3e05a07cee
JulianTs0 <juliantorresssr@gmail.com>
2026-03-23 20:59:45 -0300

    add: new errors

--- src/domain/errors/errors.ts
+++ src/domain/errors/errors.ts
@@ -18,6 +18,8 @@ export class ClientErrors {
     public static readonly INVALID_PASSWORD =
         'La contraseña debe tener al menos: ocho carácteres, una minúscula, una mayúscula y un número.';
     public static readonly INVALID_EMAIL = 'El email es inválido.';
+    public static readonly INVALID_NAME = 'El nombre es inválido.';
+    public static readonly UNAUTHORIZED = 'No autorizado.';
 }
 
 export const ApiErrors: Record<string, string> = {
