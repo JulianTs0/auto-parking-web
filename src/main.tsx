75cb9dff481fca8eb159cf40adb6698a5585928d
JulianTs0 <juliantorresssr@gmail.com>
2026-03-23 19:19:26 -0300

    add: repository & toaster injection

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import RoutesManager from './ui/routes/routes-manager'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import RoutesManager from './ui/routes/routes-manager';
+import { RepositoriesProvider } from './core';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <RoutesManager />
+        <RepositoriesProvider>
+            <RoutesManager />
+            <Toaster
+                position="bottom-left"
+                toastOptions={{ duration: 7000 }}
+            />
+        </RepositoriesProvider>
     </StrictMode>,
-)
+);
