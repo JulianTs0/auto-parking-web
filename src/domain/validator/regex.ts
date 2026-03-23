dedadef22cd5aacc62455976a9b72b9292c0da8e
JulianTs0 <juliantorresssr@gmail.com>
2026-03-23 19:23:32 -0300

    add: email regex

--- src/domain/validator/regex.ts
+++ src/domain/validator/regex.ts
@@ -1,5 +1,6 @@
 export class Regex {
-
     public static readonly EMAIL = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
+
+    public static readonly PASSWORD =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S{8,}$/;
 }
