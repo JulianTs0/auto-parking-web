a62bac79cd46632e1efaf5cfc7e8cae2c952ed11
JulianTs0 <juliantorresssr@gmail.com>
2026-03-23 20:59:52 -0300

    add: new regex

--- src/domain/validator/regex.ts
+++ src/domain/validator/regex.ts
@@ -3,4 +3,7 @@ export class Regex {
 
     public static readonly PASSWORD =
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S{8,}$/;
+
+    public static readonly FULLNAME =
+        /^[\p{L}]{2,20}(?:[ '-][\p{L}]{2,20})*$/u;
 }
