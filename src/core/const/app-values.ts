272e3008d315ec1a2b9fbc179263256c90de4bec
JulianTs0 <juliantorresssr@gmail.com>
2026-03-23 19:22:03 -0300

    add: toast messgaes

---
+++ src/core/const/app-values.ts
@@ -0,0 +1,8 @@
+export const CONSTANTS = {
+    SELECT_OPTION_VALUE: 'Seleccionar',
+    LOADING_SESSION: 'Iniciando sesión...',
+    SUCCESS_SESSION: '¡Sesión iniciada correctamente!',
+
+    LOADING_REGISTER: 'Registrando usuario...',
+    SUCCESS_REGISTER: '¡Usuario registrado correctamente!',
+};
